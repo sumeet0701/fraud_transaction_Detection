16c76cd8dbe8e8d172330acae108f55ef64bb1f3
sumeet <maheshwarisumeet128@gmail.com>
2023-07-27 13:06:47 +0530

    Execption file is created.

---
+++ fraud/exception/__init__.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+
+class FraudException(Exception):
+    def __init__(self, error_message: Exception, error_details:sys):
+        self.error_message = FraudException.get_detailed_error_message(error_message=error_message, error_details=error_details)
+        #self.error_details = error_details
+
+
+    def get_detailed_error_message(error_message: Exception, error_details:sys)-> str:
+        _,_, exec_tb = error_details.exc_info()
+        Exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"""
+                        error occured in script: 
+                        [{file_name}] at 
+                        try block line number: [{try_block_line_number}] execption block line number: [{Exception_block_line_number}]
+                        error message: [{error_message}]"""
+        
+
+        return error_message
+    
+    def __str__(self):
+        return self.error_message
+
+    def __repr__(self) -> str:
+        return FraudException.__name__.str()
