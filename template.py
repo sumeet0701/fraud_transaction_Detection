f9dabc261e47753467278056dd16f9d28ed6ce01
sumeet <maheshwarisumeet128@gmail.com>
2023-10-01 18:46:03 +0530

    Template file updated.

--- template.py
+++ template.py
@@ -36,7 +36,7 @@ list_files = [
     f"{project_name}/logger/__init__.py",
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/utils/__init__.py",
-    f"{project_name}/utils/utils.py"
+    f"{project_name}/utils/utils.py",
     f"config/config.yaml",
     "schema.yaml",
     "requirements.txt",
